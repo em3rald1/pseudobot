e00147149aaa90cd143b1096803835a1657684a4
em3rald1 <63785826+em3rald1@users.noreply.github.com>
2022-07-14 15:17:07 +0200

    Add files via upload

--- index.py
+++ index.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import random
-import string
 import keyboard
 
 code = input("Code $ ")
@@ -22,6 +21,7 @@ random.shuffle(stringone)
 used = []
 prev = ''
 curr = ''
+is_dormant = False
 
 def find_word(syll):
     for i in stringone:
@@ -31,14 +31,12 @@ def find_word(syll):
                 return i
 
 answered = []
-
 while not keyboard.is_pressed("ctrl+z"):
     try:
-        if driver.find_element('xpath', "//div[@id='GameContainer']//div[@id='JoinGameContainer']/button[@id='JoinGameButton']").text == 'Join game':
+        if driver.find_element('xpath', "//div[@id='GameContainer']//div[@id='JoinGameContainer']/button[@id='JoinGameButton']").text == 'Join game' and not is_dormant:
             driver.find_element('xpath', "//div[@id='GameContainer']//div[@id='JoinGameContainer']/button[@id='JoinGameButton']").click()
     except:
         pass
-    time.sleep(.05)
     try:
         executioner : str = driver.find_element('xpath', "//div[@id='GameContainer']//div[@id='StatusContainer']").text
         executioner = executioner.split(' ')[0][:-1]
@@ -48,6 +46,7 @@ while not keyboard.is_pressed("ctrl+z"):
         data = driver.find_element("xpath", "//div[@id='GameContainer']//div[@id='StatusContainer']//span[@class='Prompt']").text
     except:
         pass
+    time.sleep(0.05)
     try:
         curr = data
         if prev != curr:
@@ -56,10 +55,14 @@ while not keyboard.is_pressed("ctrl+z"):
     except:
         pass
     try:
-        if driver.find_element('xpath', "//div[@id='GameContainer']//div[@id='WordInputContainer']").get_attribute('class') != 'Hidden':
-            dat = find_word(curr)
-            driver.find_element('xpath', "//div[@id='GameContainer']//div[@id='WordInputContainer']//input[@id='WordInput']").send_keys(dat)
-            driver.find_element('xpath', "//div[@id='GameContainer']//div[@id='WordInputContainer']//input[@id='WordInput']").send_keys(Keys.ENTER)
+        if not is_dormant:
+            if driver.find_element('xpath', "//div[@id='GameContainer']//div[@id='WordInputContainer']").get_attribute('class') != 'Hidden':
+                dat = find_word(curr)
+                driver.find_element('xpath', "//div[@id='GameContainer']//div[@id='WordInputContainer']//input[@id='WordInput']").send_keys(dat)
+                driver.find_element('xpath', "//div[@id='GameContainer']//div[@id='WordInputContainer']//input[@id='WordInput']").send_keys(Keys.ENTER)
+        else:
+            if driver.find_element('xpath', "//div[@id='GameContainer']//div[@id='WordInputContainer']").get_attribute('class') != 'Hidden':
+                driver.find_element('xpath', "//div[@id='GameContainer']//button[@id='GlennButton']").click()
     except:
         pass
     try:
@@ -77,6 +80,8 @@ while not keyboard.is_pressed("ctrl+z"):
             fusion = '[{}]<{}>{} [id: "{}"]'.format(lm_time, lm_author, lm_cntt, last_msg.id)
             if not fusion in answered:
                 answered.append(fusion)
+                if not lm_cntt.startswith('.'):
+                    client
                 if lm_cntt.startswith('.'):
                     spl = lm_cntt[1:].split(' ')
                     cmd = spl[0]
@@ -90,11 +95,18 @@ while not keyboard.is_pressed("ctrl+z"):
                         chatInput.click()
                         chatInput.send_keys('[Runned by: {}] {}'.format(lm_author, lm_cntt[len(cmd)+2:]))
                         chatInput.send_keys(Keys.ENTER)
-                    
-            
-            
+                    if cmd == 'dormant':
+                        owner = driver.find_element('xpath', "//ul[@id='UserList']//li[@class='UsersTabUser Host']").get_attribute('data-display-name')
+                        if lm_author == owner:
+                            is_dormant = not is_dormant
+                            chatInput.click()
+                            chatInput.send_keys('[MOD-Runned by: {}] Dormant mode is {}'.format(owner, 'on' if is_dormant else 'off'))
+                            chatInput.send_keys(Keys.ENTER)
+                        else:
+                            chatInput.click()
+                            chatInput.send_keys('[Runned by: {}] You don\'t have access to this command'.format(lm_author))
+                            chatInput.send_keys(Keys.ENTER)
     except:
         pass
-
 
 driver.close()
