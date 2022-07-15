05858ad2ad3b1a55e2733b0c4110cec01446be61
em3rald1 <63785826+em3rald1@users.noreply.github.com>
2022-07-15 18:39:51 +0200

    Add files via upload

--- index.py
+++ index.py
@@ -4,14 +4,22 @@ from selenium.webdriver.common.by import By
 import time
 import random
 import keyboard
+import sys
+import subprocess
+import string
+import secrets
 
-code = input("Code $ ")
+code = sys.argv[1]
 
 options = webdriver.ChromeOptions() 
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 driver = webdriver.Chrome(options=options)
+driver.get('https://pseudo.inanimated.xyz')
+driver.add_cookie({"name": "darkMode", "value": "true"})
+driver.add_cookie({"name": "PseudoBP.sid", "value": "s%3AuvC5b9dwEKtQFnYb_gn4Ag3qTqkfZ5P0.N2nHuxDFlZTPxSPrOVJfUkv8X%2BUQl4%2BZl25%2BPYvmphc"})
 driver.get("https://pseudo.inanimated.xyz/play/{}".format(code))
+
 
 path = "./data.txt"
 wordlistone = open(path)
@@ -21,7 +29,7 @@ random.shuffle(stringone)
 used = []
 prev = ''
 curr = ''
-is_dormant = False
+is_dormant = True
 
 def find_word(syll):
     for i in stringone:
@@ -80,8 +88,6 @@ while not keyboard.is_pressed("ctrl+z"):
             fusion = '[{}]<{}>{} [id: "{}"]'.format(lm_time, lm_author, lm_cntt, last_msg.id)
             if not fusion in answered:
                 answered.append(fusion)
-                if not lm_cntt.startswith('.'):
-                    client
                 if lm_cntt.startswith('.'):
                     spl = lm_cntt[1:].split(' ')
                     cmd = spl[0]
@@ -96,8 +102,9 @@ while not keyboard.is_pressed("ctrl+z"):
                         chatInput.send_keys('[Runned by: {}] {}'.format(lm_author, lm_cntt[len(cmd)+2:]))
                         chatInput.send_keys(Keys.ENTER)
                     if cmd == 'dormant':
+                        mods = [i.get_attribute('data-display-name') for i in driver.find_elements('xpath', "//ul[@id='UserList']//li[@class='UsersTabUser Mod']")]
                         owner = driver.find_element('xpath', "//ul[@id='UserList']//li[@class='UsersTabUser Host']").get_attribute('data-display-name')
-                        if lm_author == owner:
+                        if lm_author == owner or lm_author in mods:
                             is_dormant = not is_dormant
                             chatInput.click()
                             chatInput.send_keys('[MOD-Runned by: {}] Dormant mode is {}'.format(owner, 'on' if is_dormant else 'off'))
@@ -106,6 +113,42 @@ while not keyboard.is_pressed("ctrl+z"):
                             chatInput.click()
                             chatInput.send_keys('[Runned by: {}] You don\'t have access to this command'.format(lm_author))
                             chatInput.send_keys(Keys.ENTER)
+                    if cmd == 'leave':
+                        owner = driver.find_element('xpath', "//ul[@id='UserList']//li[@class='UsersTabUser Host']").get_attribute('data-display-name')
+                        if lm_author == owner:
+                            chatInput.click()
+                            chatInput.send_keys('cya guys!')
+                            chatInput.send_keys(Keys.ENTER)
+                            driver.close()
+                            sys.exit()
+                        else:
+                            chatInput.send_keys('[Runned by: {}] You don\'t have access to this command'.format(lm_author))
+                            chatInput.send_keys(Keys.ENTER)
+                    if cmd == 'b':
+                        data = ''.join(secrets.choice(string.ascii_letters + string.digits) for x in range(6))
+                        chatInput.click()
+                        chatInput.send_keys('[Runned by: {}] https://pseudo.inanimated.xyz/{}'.format(lm_author, data))
+                        chatInput.send_keys(Keys.ENTER)
+                        subprocess.call(['python', 'index.py', data])
+                    if cmd == 'start':
+                        #print(driver.find_elements('xpath', "//ul[@id='UserList']//li[@class='UsersTabUser Mod']"))
+                        mods = [i.get_attribute('data-display-name') for i in driver.find_elements('xpath', "//ul[@id='UserList']//li[@class='UsersTabUser Mod']")]
+                        owner = driver.find_element('xpath', "//ul[@id='UserList']//li[@class='UsersTabUser Host']").get_attribute('data-display-name')
+                        print(mods, owner)
+                        if lm_author in mods or lm_author == owner:
+                            startbtncnt = driver.find_element('xpath', "//div[@id='GameContainer']//div[@id='StartGameContainer']")
+                            print(startbtncnt.get_attribute('style'))
+                            if startbtncnt.get_attribute('style') == 'display: none;':
+                                chatInput.click()
+                                chatInput.send_keys('[Runned by: {}] Unable to start the game'.format(lm_author))
+                                chatInput.send_keys(Keys.ENTER)
+                            else:
+                                startbtncnt.find_element('xpath', "//button[@id='StartGameButton']").click()
+                        else:
+                            chatInput.click()
+                            chatInput.send_keys('[Runned by: {}] You don\'t have access to this command'.format(lm_author))
+                            chatInput.send_keys(Keys.ENTER)
+                            
     except:
         pass
 
